1fe6ba3d15045962c2b7626333813c45f44bb439
Stef van Esch <stef@marshmallow.dev>
2023-09-21 11:35:36 +0200

    Remove bold text en resource ID

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -3,12 +3,11 @@
         class="flex flex-col px-6 py-2 -mx-6 space-y-2 border-t border-gray-100 md:flex-row md:py-0 md:space-y-0 dark:border-gray-700"
     >
         <div class="md:w-1/4 md:py-3">
-            <h4 class="font-bold md:font-normal">
+            <h4 class="">
                 <span v-text="field.name"></span>
             </h4>
         </div>
         <div class="break-all md:w-3/4 md:py-3 lg:break-words">
-            {{ resourceId }}
             <ButtonComponent
                 :field="field"
                 :resourceName="resourceName"
